f072e841d4f9162c0944585bea32cabeeb9592e7
sergey <sergey.krimon@axiadigitalsolutions.com>
2020-08-04 09:56:39 -0300

    FakerData

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Bogus;
+
+namespace FakerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var peoples = new Faker<Peoples>("pt_BR")
+               .RuleFor(c => c.Id, f => f.Random.Guid())
+               .RuleFor(c => c.Name, f => f.Name.FullName())
+               .RuleFor(c => c.Email, f => f.Internet.Email())
+               .RuleFor(c => c.PhoneNumber, f => f.Phone.PhoneNumber())
+               .RuleFor(c => c.Password, f => f.Internet.Password())
+               .Generate(3);
+
+            foreach (var peopleFake in peoples)
+            {
+                Console.WriteLine(@"People's Data");
+                Console.WriteLine($"ID: {peopleFake.Id}");
+                Console.WriteLine($"Nome: {peopleFake.Name}");
+                Console.WriteLine($"Email: {peopleFake.Email}");
+                Console.WriteLine($"PhoneNumber: {peopleFake.PhoneNumber}");
+                Console.WriteLine($"Password: {peopleFake.Password}");
+                Console.WriteLine("------------------");
+            }
+        }
+    }
+}
---
+++ READE.md
@@ -0,0 +1,5 @@
+FakerData C# with Bogus
+
+References:
+
+https://github.com/bchavez/Bogus
